550c699f052fab803edefcefac3ab0e8daac3d9a
Deffitzit <pavlik.volkov.0606@mail.ru>
2024-12-10 20:21:30 +0400

    new file:   assets/--.svg 	new file:   assets/-_.svg 	modified:   assets/1-.svg 	modified:   assets/10-.svg 	modified:   assets/10_.svg 	modified:   assets/11-.svg 	modified:   assets/11_.svg 	modified:   assets/12-.svg 	modified:   assets/12_.svg 	modified:   assets/13-.svg 	modified:   assets/13_.svg 	modified:   assets/14-.svg 	modified:   assets/14_.svg 	modified:   assets/15-.svg 	modified:   assets/15_.svg 	modified:   assets/16-.svg 	modified:   assets/16_.svg 	modified:   assets/17-.svg 	modified:   assets/17_.svg 	modified:   assets/18-.svg 	modified:   assets/18_.svg 	modified:   assets/19-.svg 	modified:   assets/19_.svg 	modified:   assets/1_.svg 	modified:   assets/2-.svg 	modified:   assets/20-.svg 	modified:   assets/20_.svg 	modified:   assets/21-.svg 	modified:   assets/21_.svg 	modified:   assets/22-.svg 	modified:   assets/22_.svg 	modified:   assets/23-.svg 	modified:   assets/23_.svg 	modified:   assets/24-.svg 	modified:   assets/24_.svg 	modified:   assets/25-.svg 	modified:   assets/25_.svg 	modified:   assets/26-.svg 	modified:   assets/26_.svg 	modified:   assets/27-.svg 	modified:   assets/27_.svg 	modified:   assets/28-.svg 	modified:   assets/28_.svg 	modified:   assets/29-.svg 	modified:   assets/29_.svg 	modified:   assets/2_.svg 	modified:   assets/3-.svg 	modified:   assets/30-.svg 	modified:   assets/30_.svg 	modified:   assets/31-.svg 	modified:   assets/31_.svg 	modified:   assets/32-.svg 	modified:   assets/32_.svg 	modified:   assets/33-.svg 	modified:   assets/33_.svg 	modified:   assets/3_.svg 	modified:   assets/4-.svg 	modified:   assets/4_.svg 	modified:   assets/5-.svg 	modified:   assets/5_.svg 	modified:   assets/6-.svg 	modified:   assets/6_.svg 	modified:   assets/7-.svg 	modified:   assets/7_.svg 	modified:   assets/8-.svg 	modified:   assets/8_.svg 	modified:   assets/9-.svg 	modified:   assets/9_.svg 	new file:   assets/_-.svg 	new file:   assets/__.svg 	new file:   assets/scobs.svg 	modified:   css/styles.css 	modified:   css/styles.css.map 	modified:   html/trtrtrtrtr.html 	modified:   js/script.js 	modified:   scss/styles.scss

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ const inputFrame = document.getElementById("input_frame");
 
 const flipScreenCount = 15;
 
-const alphabet = 'АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ-'
+const alphabet = ' -АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ'
 
 function TopTerminal(count) {
     for (let i = 0; i < count; i++) {
@@ -32,8 +32,6 @@ function handleInput(terminalClass, displayFunction) {
                 for (let i = 0; i < flipScreenCount; i++) {
                     if (signArr[i] !== undefined && /^[A-Za-zА-Яа-яЁё-]$/.test(signArr[i])) {
                         displayFunction(getTerminalImg[i], signArr[i]);
-                    } else {
-                        getTerminalImg[i].innerHTML = "";
                     }
                 }
             }
@@ -42,46 +40,69 @@ function handleInput(terminalClass, displayFunction) {
 }
 
 function displayTopLetter(divElement, targetLetter) {
-    const svgFiles = ['/assets/1-.svg', '/assets/2-.svg', '/assets/3-.svg', '/assets/4-.svg', '/assets/5-.svg', '/assets/6-.svg', '/assets/7-.svg', 
-        '/assets/8-.svg', '/assets/9-.svg', '/assets/10-.svg', '/assets/11-.svg', '/assets/12-.svg', '/assets/13-.svg', '/assets/14-.svg', 
-        '/assets/15-.svg', '/assets/16-.svg', '/assets/17-.svg', '/assets/18-.svg', '/assets/19-.svg', '/assets/20-.svg', '/assets/21-.svg', 
-        '/assets/22-.svg', '/assets/23-.svg', '/assets/24-.svg', '/assets/25-.svg', '/assets/26-.svg', '/assets/27-.svg', '/assets/28-.svg', 
-        '/assets/29-.svg', '/assets/30-.svg', '/assets/31-.svg', '/assets/32-.svg', '/assets/33-.svg'];
+    const svgFiles = ['/assets/_-.svg', '/assets/--.svg', '/assets/1-.svg', '/assets/2-.svg', '/assets/3-.svg', '/assets/4-.svg', '/assets/5-.svg', 
+        '/assets/6-.svg', '/assets/7-.svg', '/assets/8-.svg', '/assets/9-.svg', '/assets/10-.svg', '/assets/11-.svg', 
+        '/assets/12-.svg', '/assets/13-.svg', '/assets/14-.svg', '/assets/15-.svg', '/assets/16-.svg', 
+        '/assets/17-.svg', '/assets/18-.svg', '/assets/19-.svg', '/assets/20-.svg', '/assets/21-.svg', 
+        '/assets/22-.svg', '/assets/23-.svg', '/assets/24-.svg', '/assets/25-.svg', '/assets/26-.svg', 
+        '/assets/27-.svg', '/assets/28-.svg', '/assets/29-.svg', '/assets/30-.svg', '/assets/31-.svg', 
+        '/assets/32-.svg', '/assets/33-.svg'];
 
     let index = 0;
-    const interval = setInterval(() => {
-        divElement.innerHTML = `<img class = "term-svg" src="${svgFiles[index % svgFiles.length]}"/>`;
-        alphabetLetter = alphabet[index % alphabet.length]
-        index++;
+    let found = false;
 
+    const interval = setInterval(() => {
+        divElement.innerHTML = `<img class="term-svg" src="${svgFiles[index]}"/>`;
+        const alphabetLetter = alphabet[index % alphabet.length];
 
         if (alphabetLetter === targetLetter.toUpperCase()) {
-            clearInterval(interval)
+            found = true;
+            clearInterval(interval);
+        }
+
+        index++;
+
+        if (index >= svgFiles.length && !found) {
+            divElement.innerHTML = `<img class="term-svg" src="/assets/_-.svg"/>`;
+            clearInterval(interval);
         }
     }, 100);
 }
 
 function displayBottomLetter(divElement, targetLetter) {
-    const svgFiles = ['/assets/1_.svg', '/assets/2_.svg', '/assets/3_.svg', '/assets/4_.svg', '/assets/5_.svg', '/assets/6_.svg', '/assets/7_.svg', 
-        '/assets/8_.svg', '/assets/9_.svg', '/assets/10_.svg', '/assets/11_.svg', '/assets/12_.svg', '/assets/13_.svg', '/assets/14_.svg', 
-        '/assets/15_.svg', '/assets/16_.svg', '/assets/17_.svg', '/assets/18_.svg', '/assets/19_.svg', '/assets/20_.svg', '/assets/21_.svg', 
-        '/assets/22_.svg', '/assets/23_.svg', '/assets/24_.svg', '/assets/25_.svg', '/assets/26_.svg', '/assets/27_.svg', '/assets/28_.svg', 
-        '/assets/29_.svg', '/assets/30_.svg', '/assets/31_.svg', '/assets/32_.svg', '/assets/33_.svg'];
+    const svgFiles = ['/assets/__.svg', '/assets/-_.svg', '/assets/1_.svg', '/assets/2_.svg', '/assets/3_.svg', '/assets/4_.svg', '/assets/5_.svg',
+        '/assets/6_.svg', '/assets/7_.svg', '/assets/8_.svg', '/assets/9_.svg', '/assets/10_.svg', '/assets/11_.svg',
+        '/assets/12_.svg', '/assets/13_.svg', '/assets/14_.svg', '/assets/15_.svg', '/assets/16_.svg',
+        '/assets/17_.svg', '/assets/18_.svg', '/assets/19_.svg', '/assets/20_.svg', '/assets/21_.svg', 
+        '/assets/22_.svg', '/assets/23_.svg', '/assets/24_.svg', '/assets/25_.svg', '/assets/26_.svg',
+        '/assets/27_.svg', '/assets/28_.svg', '/assets/29_.svg', '/assets/30_.svg', '/assets/31_.svg',
+        '/assets/32_.svg', '/assets/33_.svg'];
     
-    let index = 0;
-    const interval = setInterval(() => {
-        divElement.innerHTML = `<img class = "term-svg" src="${svgFiles[index % svgFiles.length]}"/>`;
-        alphabetLetter = alphabet[index % alphabet.length]
-        index++;
+        let index = 0;
+        let found = false;
+    
+        const interval = setInterval(() => {
+            divElement.innerHTML = `<img class="term-svg" src="${svgFiles[index]}"/>`;
+            const alphabetLetter = alphabet[index % alphabet.length];
 
+            if (alphabetLetter === targetLetter.toUpperCase()) {
+                found = true;
+                clearInterval(interval);
+            }
 
-        if (alphabetLetter === targetLetter.toUpperCase()) {
-            clearInterval(interval)
-        }
-    }, 100);
+            index++;
+
+            if (index >= svgFiles.length && !found) {
+                divElement.innerHTML = `<img class="term-svg" src="/assets/__.svg"/>`;
+                clearInterval(interval);
+            }
+
+        }, 100);
 }
 
+document.addEventListener("DOMContentLoaded", function() {
     TopTerminal(flipScreenCount);
     BottomTerminal(flipScreenCount);
     handleInput("topterminal_img", displayTopLetter);
     handleInput("bottomterminal_img", displayBottomLetter);
+});
